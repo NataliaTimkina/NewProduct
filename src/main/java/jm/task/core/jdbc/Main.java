d395c060299fce52374667a9bcefecc4c8581c76
NataliaTimkina <natasha-kari@mail.ru>
2024-06-09 21:17:58 +0300

    make hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,23 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.dropUsersTable();
         userService.createUsersTable();
-       userService.saveUser("Мария", "Тимкина", (byte) 45);
-      // userService.saveUser("Тимофей", "Петров", (byte) 25);
-        //userService.saveUser("Максим", "Красоткин", (byte) 30);
-      // userService.saveUser("Наталья", "Иванова", (byte) 68);
+        userService.saveUser("Мария", "Тимкина", (byte) 45);
+        userService.saveUser("Тимофей", "Петров", (byte) 25);
+        userService.saveUser("Максим", "Васильев", (byte) 30);
+        userService.saveUser("Наталья", "Иванова", (byte) 68);
+
+        List<User> userList = userService.getAllUsers();
+        userList.forEach(System.out::println);
 
         //userService.removeUserById(2);
         userService.getAllUsers().forEach(System.out::println);
         //userService.cleanUsersTable();
-
-
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,14 @@
 package jm.task.core.jdbc.util;
 
-import com.mysql.cj.xdevapi.SessionFactory;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -15,6 +17,7 @@ import static java.lang.System.getProperties;
 
 public class Util {
 
+    //Настройка соединения через JDBC
     public static Connection connect() {
         Properties properties = new Properties();
         try {
@@ -41,19 +44,34 @@ public class Util {
         }
     }
 
-    //public static SessionFactory setUp() {
-    //    Properties properties = getProperties();
-/*
+    //Настройка соединения через Hibernate
+    public static SessionFactory setUp() {
+        Properties properties = getProperties();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .applySetting("hibernate.connection.url", properties.getProperty("host"))
                 .applySetting("hibernate.connection.username", properties.getProperty("user"))
                 .applySetting("hibernate.connection.password", properties.getProperty("password"))
                 .applySetting("hibernate.show_sql", properties.getProperty("hibernate.show_sql"))
                 .applySetting("hibernate.format_sql", properties.getProperty("hibernate.format_sql"))
-                .applySetting("hibernate.use_sql_comments", properties.getProperty("hibernate.use_sql_comments")).build()
+                .applySetting("hibernate.use_sql_comments", properties.getProperty("hibernate.use_sql_comments"))
+                .build();
+        try {
+            Metadata metadata = new MetadataSources(registry)
+                    .addAnnotatedClass(User.class).buildMetadata();
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException("Проверьте настройки, Вы не смогли подключиться к БД");
 
 
- */
-
+        }
     }
+}
 
